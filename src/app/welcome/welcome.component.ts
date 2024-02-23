9630811b4406e9f2a4304f87fecfe7d569df5fd2
ChrisVai <chrisbellet@gmail.com>
2024-02-23 18:45:20 +0100

    Ajout Filtres + mise en place RxJs
    Ajout des filtres sur welcome + filtrage via Rxjs
    Ajout filtrage RxJs des articles par id dans article service pour view-articles

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,8 +1,11 @@
 import {Component, inject} from '@angular/core';
 import {ArticleService} from "../services/article.service";
-import {NgClass, NgOptimizedImage} from "@angular/common";
-import {Router, RouterLink} from "@angular/router";
+import {AsyncPipe, NgClass, NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 import {HeaderComponent} from "../header/header.component";
+import {combineLatest, isEmpty, map, Observable, startWith} from "rxjs";
+import {Article} from "../models/article";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-welcome',
@@ -11,13 +14,43 @@ import {HeaderComponent} from "../header/header.component";
     NgOptimizedImage,
     RouterLink,
     HeaderComponent,
-    NgClass
+    NgClass,
+    AsyncPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
+
 export class WelcomeComponent {
-  private articleService: ArticleService = inject(ArticleService);
-  private router: Router = inject(Router)
-    articles = this.articleService.articles;
+
+  private articleManager: ArticleService = inject(ArticleService);
+  private formBuilder: FormBuilder = inject(FormBuilder)
+  search = this.formBuilder.nonNullable.group({
+    searchArticle: [''],
+    categoryFilter: ['']
+  })
+  articles$: Observable<Article[]> = this.getFilteredArticles()
+
+  private getFilteredArticles(): Observable<Article[]> {
+
+    const articles$: Observable<Article[]> = this.articleManager.fetchArticles();
+    const search$ = combineLatest([
+      this.search.controls.searchArticle.valueChanges.pipe(startWith('')),
+      this.search.controls.categoryFilter.valueChanges.pipe(startWith(''))
+    ])
+
+    return combineLatest([articles$,search$]).pipe(
+      map(([articles, [research,categoryFilter]]) => articles.filter(article => {
+        const researchMatching = article.title.toLowerCase().includes(research.toLowerCase());
+        const categoryFilterMatching = article.category.toLowerCase().includes(categoryFilter.toLowerCase());
+
+        return researchMatching && categoryFilterMatching;
+      }))
+    )
+  }
+
+  isEmpty(param: Observable<any[]>): Observable<boolean>{
+    return param.pipe(isEmpty())
+  }
 }
